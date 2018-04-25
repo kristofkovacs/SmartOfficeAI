b4372a2da5a1f4f5f0a88fe719d6b45f260702b4
Zsiga Tibor <tibor.zsiga@hotmail.com>
2018-04-25 20:03:58 +0200

    log üzenetbe időbélyeg

--- gui.java
+++ gui.java
@@ -1,5 +1,10 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.Calendar;
+import java.util.Date;
 
 public class GUI implements ActionListener {
 	public static final String windowTitle = "Intelligens Elosztott Rendszerek - 2018";
@@ -9,70 +14,69 @@ public class GUI implements ActionListener {
 	public static final String consolePanelTitle = "Rendszer konzol";
 
 	Window window;
-	
+
 	public GUI() {
 		window = new Window(this);
 	}
-	
-	//Szenzor adatok küldése gomb eseménykezelő
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			window.getRooms();
-			//TODO send data to env
-			
-		}
-	
-	public void log(String str) {
-		window.log(str);
+
+	// Szenzor adatok küldése gomb eseménykezelő
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		window.getRooms();
+		// TODO send data to env
+
 	}
-	
+
+	public void log(String str) {
+		String timeStamp = new SimpleDateFormat("HH:mm:ss.SSS").format(Calendar.getInstance().getTime());
+
+		String logStr = " [ " + timeStamp.toString() + " ] " + " - " + str;
+		window.log(logStr);
+	}
+
 	public void turnOnAlarm(String message) {
 		window.setAlarm(message);
 	}
-	
+
 	public void turnOffAlarm() {
 		window.setAlarm(null);
 	}
-	
+
 	public void callFireFighters() {
 		window.callFireFighers(true);
 	}
-	
+
 	public void stopCallingFireFighters() {
 		window.callFireFighers(false);
 	}
-	
+
 	public void callPolice() {
 		window.callPolice(true);
 	}
-	
+
 	public void stopCallingPolice() {
 		window.callPolice(false);
 	}
-	
+
 	public void closeDoors(int roomNumber) {
 		window.closeDoors(roomNumber, true);
 	}
-	
+
 	public void openDoors(int roomNumber) {
 		window.closeDoors(roomNumber, false);
 	}
-	
+
 	public void startingBackup(int fromRoom, int toRoom) {
 		window.startBackup(fromRoom, toRoom, true);
 	}
-	
+
 	public void backupFinished(int fromRoom, int toRoom) {
 		window.startBackup(fromRoom, toRoom, false);
 	}
-	
-	
-	//for test purposes
+
+	// for test purposes
 	public static void main(String[] args) {
-		GUI gui = new GUI();		
+		GUI gui = new GUI();
 	}
 
-	
-	
-	
 }
